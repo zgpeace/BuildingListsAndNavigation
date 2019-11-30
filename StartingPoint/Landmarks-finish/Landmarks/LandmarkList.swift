2c94800fafefe2e955fceba969b273599f9f5424
zgpeace <zgpeace@gmail.com>
2019-11-30 21:16:49 +0800

    List & Navigation

---
+++ StartingPoint/Landmarks-finish/Landmarks/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by zgpeace on 2019/11/30.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+        .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
+    }
+}
